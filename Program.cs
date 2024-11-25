d3e8140f8eb4638a93edbb5bc96f1a230e59be25
DavidLabett <david.labett@gmail.com>
2024-11-25 13:01:55 +0100

    First commit, code-example

--- Program.cs
+++ Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello Holliday!");
+        Database database = new();
+        // hämta anslutningen (db) att göra queries med
+        var db = database.Connection();
+        // skapa actions och skicka in anslutningen, så att vi kan köra queries till databasen där
+        var query = new Query(db);
+        // skapa en meny och skicka in våra actions i den, så vi kan anropa dem
+        new Menu(query);
+        
+        
     }
 }
