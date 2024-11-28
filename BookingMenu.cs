ec6b2ab63220b150d6940455dc1b6393330c0d71
DavidLabett <david.labett@gmail.com>
2024-11-28 13:35:37 +0100

    bookingmenu update

--- BookingMenu.cs
+++ BookingMenu.cs
@@ -1,47 +1,66 @@
 namespace HelloHoliday;
-
 public class BookingMenu
 {
-    public void Menu()
+    //Making sure BookingPreferences are returned in method
+    public BookingPreferences Menu()
     {
         Console.WriteLine("+------Welcome---to---Booking-------+");
         Console.WriteLine("| When would you like to check in?");
         Console.WriteLine("| (Please use format 'YYYY-MM-DD')");
         Console.Write("> ");
         string? checkIn = Console.ReadLine();
+
         Console.WriteLine("| When will you check out?");
         Console.Write("> ");
         string? checkOut = Console.ReadLine();
+
         Console.WriteLine("| What kind of room do you need?");
         Console.WriteLine("| (Single, Double, Triple or Quad?)");
         Console.Write("> ");
-        string? roomSize = Console.ReadLine().ToLower();
+        string? roomSize = Console.ReadLine()?.ToLower();
+
         Console.WriteLine("| Do you want a pool? (Yes/No)");
         Console.Write("> ");
-        string? pool = Console.ReadLine().ToLower();
-        bool wantsPool = pool == "yes"; //Convert input to bool
+        string? pool = Console.ReadLine()?.ToLower();
+
         Console.WriteLine("| Should there be entertainment options? (Yes/No)");
         Console.Write("> ");
-        string? entertainment = Console.ReadLine().ToLower();
-        bool wantsEntertainment = entertainment == "yes"; //Convert input to bool
+        string? entertainment = Console.ReadLine()?.ToLower();
+
         Console.WriteLine("| Is a kid's club important? (Yes/No)");
         Console.Write("> ");
-        string? kidsClub = Console.ReadLine().ToLower();
-        bool wantskidsClub = kidsClub == "yes"; //Convert input to bool
+        string? kidsClub = Console.ReadLine()?.ToLower();
+
         Console.WriteLine("| Do you want an on-site restaurant? (Yes/No)");
         Console.Write("> ");
-        string? restaurant = Console.ReadLine().ToLower();
-        bool wantsrestaurant = restaurant == "yes"; //Convert input to bool
+        string? restaurant = Console.ReadLine()?.ToLower();
+
         Console.WriteLine("| How far from the beach is okay (in km)");
         Console.Write("> ");
-        string? beach = Console.ReadLine().ToLower();
+        string? beach = Console.ReadLine();
+
         Console.WriteLine("| How far from the city-centre is okay (in km)");
         Console.Write("> ");
-        string? cityCentre = Console.ReadLine().ToLower();
+        string? cityCentre = Console.ReadLine();
+
         Console.WriteLine("| What matters more to you: ");
         Console.WriteLine("| Great reviews or great price?");
         Console.Write("> ");
-        string? reviewOrPrice = Console.ReadLine().ToLower();
+        string? reviewOrPrice = Console.ReadLine()?.ToLower();
+        
+        //add check that inputs are not null
+        return new BookingPreferences
+        {
+            CheckInDate = checkIn,
+            CheckOutDate = checkOut,
+            RoomSize = roomSize,
+            Pool = pool,
+            Entertainment = entertainment,
+            KidsClub = kidsClub,
+            Restaurant = restaurant,
+            DistanceToBeach = beach,
+            DistanceToCityCentre = cityCentre,
+            Preference = reviewOrPrice
+        };
     }
-    
 }
