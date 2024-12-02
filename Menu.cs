aa3373641b25c2282f31db5d80a012280005b5b3
Malin W <malin.wiren@hotmail.com>
2024-12-02 10:39:45 +0100

    added methods for getting input as a string and as a bool

---
+++ Menu.cs
@@ -0,0 +1,45 @@
+﻿namespace HelloHoliday;
+
+public class Menu
+{
+    public string GetInputAsString()
+    {
+        Console.Write("> ");
+        string? input = Console.ReadLine();
+        input = input.Trim().ToLower();
+
+        if (string.IsNullOrEmpty(input))
+        {
+            Console.WriteLine("You need to write something");
+            input = GetInputAsString();
+        }
+
+        return input;
+    }
+
+    public bool GetInputAsBool()
+    {
+        string input = GetInputAsString();
+
+        switch (input)
+        {
+            case "y":
+            case "yes":
+            case "true":
+                return true;
+            case "n":
+            case "no":
+            case "false":
+                return false;
+        }
+
+        Console.WriteLine("Please say 'yes' or 'no'");
+        return GetInputAsBool();
+    }
+
+    public int GetInputAsInt()
+    {
+        string input = GetInputAsString();
+        return 0;
+    }
+}
